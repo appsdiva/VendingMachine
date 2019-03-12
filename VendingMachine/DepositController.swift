6ffacfd25132d530dc97ad90df74acdf28b0ec10
appsdiva <stacey@appsdevo.com>
2019-03-12 16:13:51 -0600

    Initial Commit

---
+++ VendingMachine/DepositController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  VendingMachine
+//
+//  Created by Stacey Smith on 28/9/18 .
+//  Copyright © 2018 AppsDevo. All rights reserved.
+//
+
+import UIKit
+
+class DepositController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VendingMachine/VendingItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  VendingMachine
+//
+//  Created by Stacey Smith on 28/9/18 .
+//  Copyright © 2018 AppsDevo. All rights reserved.
+//
+
+import UIKit
+
+class VendingItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconView: UIImageView!
+}
